56abd04a4d3ddad71405ce118b95319f6831cc3f
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-05-17 14:58:10 +0200

    Investigate path imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { IconButton } from "@material-ui/core";
-import { Add as AddIcon } from "@material-ui/icons";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
 
 function App() {
   return (
